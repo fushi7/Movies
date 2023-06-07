73433384fb4d4a4cd4fde6af7443930a0fff0d1f
fushi7 <danielvitorgp@gmail.com>
2023-06-07 14:11:51 -0300

    commit

---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,34 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "#000000",
+    },
+    header:{                    
+      padding: 25,
+    },
+    headerText:{
+    marginTop: 30,
+    fontSize: 24,
+    lineHeight: 45,
+    color: "#FFFFFF"
+  },
+  containerInput: {
+    backgroundColor: "#24db04",
+    height: 42,
+    padding: 10,
+    borderRadius: 16,
+    marginTop: 24,
+    marginBottom: 20,
+    alignItems: "center",
+    justifyContent: "space-between",
+    flexDirection: "row"
+  },
+  input: {
+   color: "#FFFFFF", 
+   width: "80%",
+   paddingLeft: 15,
+  },
+  });
+  
